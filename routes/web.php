37d6954013f70d8acf148598df355e897f9c113a
ilham arifin <arifinn2208@gmail.com>
2024-05-17 17:03:55 +0700

    praktikum week10

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/admin', [AdminController::class, 'index']);
